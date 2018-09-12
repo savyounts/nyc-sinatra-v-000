215b447187122bbccab9ddb5f4d6a8b4dd187db7
Savannah Younts <savyounts@gmail.com>
2018-09-12 20:27:44 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,28 @@
 class FiguresController < ApplicationController
 
+  get '/figures' do 
+    @figures = Figures.all 
+    erb :index 
+  end
+
+  get '/figures/new' do 
+    erb :new 
+  end 
+
+  post '/figures' do
+    if !params[:landmark][:name].empty? 
+      @landmark = Landmark.create(name: params[:landmark][:name])
+    end 
+    if !params[:title][:name].empty?
+      @title = Title.create(name: params[:title][:name])
+    end 
+    @figure = Figure.create(params)
+
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do 
+    @figure = Figure.find(params[:id])
+
+  
 end
