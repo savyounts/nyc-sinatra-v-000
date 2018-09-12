8feca0a2244bf6108c87c2b99f07fd249e5ed824
Savannah Younts <savyounts@gmail.com>
2018-09-12 22:16:02 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180912194333) do
-
-  create_table "figure_titles", force: :cascade do |t|
-    t.integer "figure_id"
-    t.integer "title_id"
-  end
+ActiveRecord::Schema.define(version: 20180912194241) do
 
   create_table "figures", force: :cascade do |t|
     t.string "name"
