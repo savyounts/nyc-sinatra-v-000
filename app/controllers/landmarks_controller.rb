e2df18b9b3635ce79decfa2c2fb9fb93d2dfbadd
Savannah Younts <savyounts@gmail.com>
2018-09-12 22:09:39 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -10,7 +10,6 @@ class LandmarksController < ApplicationController
   end
 
   post '/landmarks' do
-    binding.pry
     @landmark = Landmark.create(params[:landmark])
 
     redirect "/landmarks/#{@landmark.id}"
