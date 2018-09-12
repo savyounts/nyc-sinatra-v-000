27ba7a6f1acc309c545e293fc9dd7c3431cbf9d1
Savannah Younts <savyounts@gmail.com>
2018-09-12 20:40:10 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,21 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+
+  get '/landmarks' do 
+    @landmarks = Landmark.all 
+    erb :'/landmarks/index'
+  end 
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  post '/landmarks' do 
+    @landmark = Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed]
+
+    redirect "/landmarks/#{@landmark.id}"
+  end 
+
+  get '/landmarks/:id' do 
+    erb :'/landmarks/show'
+  end
 end
