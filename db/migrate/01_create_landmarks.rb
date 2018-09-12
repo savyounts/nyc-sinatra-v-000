9cfa842d6e761d5beba81d494cf953358e2849df
Savannah Younts <savyounts@gmail.com>
2018-09-12 19:42:00 +0000

    automatically backed up by learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,9 +1,9 @@
 class CreateLandmarks < ActiveRecord::Migration
   raise 'Write CreateLandmarks migration here'
-  def change 
+  def change
     create_table :landmarks do |t|
-      t.string :name 
-      t.integer :figure_id 
+      t.string :name
+      t.integer :figure_id
       t.string :year_completed
     end
   end
