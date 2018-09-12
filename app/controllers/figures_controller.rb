02b3fa6980d9495fa25e4161b77d471b72e0b2a1
Savannah Younts <savyounts@gmail.com>
2018-09-12 20:32:02 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -10,6 +10,8 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
+    @landmarks = Landmarks.all 
+    @titles = Titles.all
     if !params[:landmark][:name].empty?
       @landmark = Landmark.create(name: params[:landmark][:name])
     end
